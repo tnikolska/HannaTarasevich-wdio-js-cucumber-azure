2a62a00e960da1f61637563f54f57ca813337fbd
Hanna T <ann.tarasevich@gmail.com>
2022-10-21 21:07:50 +0200

    Initial commit

---
+++ features/login.feature
@@ -0,0 +1,26 @@
+Feature: The Internet Guinea Pig Website
+
+  @C5
+  Scenario: As a user I can log into the secure area
+    Expected to be failed
+
+    Given I am on the login page
+    When I login with <username> and <password>
+    Then I should see a flash message saying <message>
+
+    Examples:
+      | username | password             | message                         |
+      | tomsmith | SuperSecretPassword! | You logged into a secure area!! |
+
+
+  @C9
+  Scenario: As a user I can log into the secure area 2
+    Expected to be passed
+
+    Given I am on the login page
+    When I login with <username> and <password>
+    Then I should see a flash message saying <message>
+
+    Examples:
+      | username | password             | message                        |
+      | tomsmith | SuperSecretPassword! | You logged into a secure area! |
---
+++ utils/azure-reporting.js
@@ -0,0 +1,221 @@
+const axios = require('axios').default;
+const azureConfig = require('../azure-config.json');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
+const fs = require('fs');
+
+let reportFileName = fs.readdirSync('./reports/cucumberJS');
+
+const report = require(`../reports/cucumberJS/${reportFileName}`);
+
+const azure = axios.create({
+  baseURL: azureConfig.organizationUrl,
+  timeout: 100*1000,
+  headers: {
+    'Authorization': `Basic ${Buffer.from(`PAT:${argv.pat}`).toString('base64')}`,
+    'Content-Type': 'application/json'
+  }
+});
+
+const getTestCasesFromTestSuite = async function() {
+  let items = [];
+  const response = await azure.get(`testplan/Plans/${azureConfig.planId}/Suites/${azureConfig.suiteId}/TestPoint`);
+  switch (response.status) {
+      case 404:
+          console.log(`Error: ${response.data.message}`);
+          break;
+      case 200:
+        items = [...response.data.value];
+          if (items.length === 0) {
+              console.log(`Error: Suite ${azureConfig.suiteId} does not have Test Points`);
+          }
+          break;
+      default:
+          console.log('Error:', response.data);
+  }
+
+  return items;
+}
+
+const resetCasesStatusToActive = async function() {
+  let items = await getTestCasesFromTestSuite();
+  
+  let requestBody = [];
+
+  for(let elem of items){
+    requestBody.push({
+      "id": elem.id,  
+      "isActive": true
+    })
+  };
+
+  const response = await azure.patch(`testplan/Plans/${azureConfig.planId}/Suites/${azureConfig.suiteId}/TestPoint?includePointDetails=true&returnIdentityRef=true&api-version=6.0`, requestBody);
+  switch (response.status) {
+    case 404:
+        console.log(`Error: ${response.data.message}`);
+        break;
+    case 200:
+      items = [...response.data.value];
+      console.log(`Reset "${items.length}" Test Point(s) in Suite ${azureConfig.suiteId} to Active`);
+      break;
+    default:
+        console.log('Error:', response.data);
+
+  return response;
+}
+}
+
+const parseJsonReport = async function() {
+  let testsResults = [];
+
+  for (let elem of report[0].elements) {
+    let scenarioStatus;
+    let testCaseId;
+    let screenshot;
+    let errorMessage;
+    let name;
+
+    testCaseId = elem.tags[0].name.slice(2);
+    name = elem.name;
+    const isAnyStepFailed = elem.steps.some(step => step.result.status === 'failed');
+  
+    if (isAnyStepFailed) {
+      for (let step of elem.steps) {
+        if (step.result.status === 'failed') {
+          errorMessage = step.result.error_message;
+        }
+        if(step.hasOwnProperty('embeddings'))
+          screenshot = step.embeddings[0].data;
+      }
+    }
+    scenarioStatus = isAnyStepFailed ? 'failed' : 'passed';
+    testsResults.push({
+      id: testCaseId,
+      name,
+      scenarioStatus,
+      screenshot,
+      errorMessage
+      })
+  }
+  return testsResults;
+}
+
+const getRunID = async function() {
+  const response = await azure.get('test/runs?api-version=6.0');
+  let runId = response.data.value.filter(item => item.name === azureConfig.runName);
+
+  return runId[runId.length - 1].id;
+}
+
+const getRunTestResults = async function () {
+  const runId = await getRunID();
+  const response = await azure.get(`test/runs/${runId}/results?api-version=6.0`);
+
+  return response;
+}
+
+const updateRunResultsWithErrors = async function() {
+  let runId = await getRunID();
+  let runResults = await parseJsonReport();
+  let azureTestRunResult = (await getRunTestResults()).data.value;
+
+  let requestBody = [];
+
+  for(let item of azureTestRunResult){
+    for (let result of runResults) {
+      if (item.testCase.name === result.name && result.errorMessage) {
+        requestBody.push({
+          "id": item.id,  
+          "errorMessage": result.errorMessage
+        })
+      }
+    }
+  };
+
+  const response = await azure.patch(`test/runs/${runId}/results?api-version=5.0`, requestBody);
+  switch (response.status) {
+    case 404:
+        console.log(`Error: ${response.data.message}`);
+        break;
+    case 200:
+      console.log(`Error messages in "${[...response.data.value].length}" Test Point(s) in Run ${runId} were updated`);
+      break;
+    default:
+        console.log('Error:', response.data);
+}
+
+  return response;
+}
+
+const updateRunResultsWithScreenshot = async function() {
+  let runId = await getRunID();
+  let runResults = await parseJsonReport();
+  let azureTestRunResult = (await getRunTestResults()).data.value;
+  let response; 
+  let requestBody; 
+
+  for(let item of azureTestRunResult){
+    for (let result of runResults) {
+      if (item.testCase.name === result.name && result.screenshot) {
+
+        requestBody = {
+        "fileName": "screenshot.png",
+        "comment": "Test attachment upload",
+        "attachmentType": "GeneralAttachment",
+        "stream": `${result.screenshot}`
+        };
+
+        response = await azure.post(`test/runs/${runId}/results/${item.id}/attachments?api-version=5.1-preview.1`, requestBody);
+        
+        if (response.status !== 200) {
+          console.log(`Error: ${response.data}`);
+        }
+      }
+    }
+  }
+}
+
+const updateTestSuiteWithResults = async function() {
+    let items = await getTestCasesFromTestSuite();
+    let runResults = await parseJsonReport();
+
+    let requestBody = [];
+
+    for (let item of items) {
+      let result = runResults.filter(result => result.id == item.testCaseReference.id);
+      
+      if (result.length > 0) {
+        requestBody.push({
+            "id": item.id,
+            "results": {
+                "outcome": result[0]?.scenarioStatus
+            }
+          });
+      } else {
+        console.log(`@C${item.testCaseReference.id} case does not have the result. Probably, it does not exist in report.`)
+      }
+  }
+  const response = await azure.patch(`testplan/Plans/${azureConfig.planId}/Suites/${azureConfig.suiteId}/TestPoint?includePointDetails=true&returnIdentityRef=true&api-version=6.0`, requestBody);
+  switch (response.status) {
+    case 404:
+        console.log(`Error: ${response.data.message}`);
+        break;
+    case 200:
+      console.log(`Statuses in Suite ${azureConfig.suiteId} were updated`);
+      break;
+    default:
+        console.log('Error:', response.data);
+
+  return response;
+  }
+}
+
+const uploadReport = async function() {
+  await resetCasesStatusToActive();
+  await updateRunResultsWithErrors();
+  await updateRunResultsWithScreenshot();
+  await updateTestSuiteWithResults();
+}
+
+uploadReport();
